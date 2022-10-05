d283f90823b8f28eef592ada3f479dca491d7e58
anna-ermakova <korovanna8@mail.ru>
2022-10-05 14:53:49 +0300

    Задания 1-3

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,34 @@
 public class Main {
     public static void main(String[] args) {
+        // task1
+        System.out.println("Задание 1.");
+        //С помощью цикла for выведите в консоль все числа от 1 до 10.
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
 
+        // task2
+        System.out.println("Задание 2.");
+        //С помощью цикла for выведите в консоль все числа от 10 до 1.
+        for (int i=10; i>=1; i--){
+            System.out.println(i);
+        }
 
+        // task3
+        System.out.println("Задание 3.");
+        //Выведите в консоль все четные числа от 0 до 17.
+        for (int i=0; i<=17; i=i+2) {
+            System.out.println(i);
+        }
     }
 }
+
+
+
+
+
+
+
 
 
 
