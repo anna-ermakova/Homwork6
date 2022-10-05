31c37dba4cb59a9d38727f8a87f9f188d241db64
anna-ermakova <korovanna8@mail.ru>
2022-10-05 15:00:43 +0300

    Задания 1-4

--- src/Main.java
+++ src/Main.java
@@ -17,11 +17,18 @@ public class Main {
         // task3
         System.out.println("Задание 3.");
         //Выведите в консоль все четные числа от 0 до 17.
-        for (int i=0; i<=17; i=i+2) {
+        for (int i=0+2; i<=17; i=i+2) {
             System.out.println(i);
         }
+        // task4
+        System.out.println("Задание 4.");
+        //Выведите в консоль все числа от 10 до - 10 от бОльшего числа к меньшему.
+        for (int i=10; i>=-10; i--){
+            System.out.println(i);
+        }
+        }
     }
-}
+
 
 
 
